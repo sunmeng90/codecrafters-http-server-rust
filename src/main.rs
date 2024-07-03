752302e13811b4a601144778318451a462f13954
Meng <sunmeng90@126.com>
2024-07-03 22:26:02 +0800

    1. respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,8 +9,9 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
             }
             Err(e) => {
                 println!("error: {}", e);
