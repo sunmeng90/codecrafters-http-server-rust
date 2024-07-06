2e62a2e4017da8de4713bd5fcc1ef0d9e5ecdd5e
Meng <sunmeng90@126.com>
2024-07-06 17:35:26 +0800

    2. Extract URL path: improve err handling

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,28 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let mut buf = [0; 512];
-                _stream.read(&mut buf).unwrap();
-                let path = String::from_utf8_lossy(&buf[..])
-                    .lines()
-                    .next()
-                    .expect("should have first line in request")
-                    .splitn(3, " ")
-                    .skip(1)
-                    .next()
-                    .to_owned()
-                    .unwrap()
-                    .to_owned();
-                if path == "/" {
-                    _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
-                } else {
-                    _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").expect("200 \n");
-                }
+                let mut buf = [0; 1024];
+                match _stream.read(&mut buf) {
+                    Ok(bytes_read) if bytes_read > 0 => {
+                        let path = String::from_utf8_lossy(&buf[..bytes_read])
+                            .lines()
+                            .next()
+                            .expect("should have first line in request")
+                            .splitn(3, " ")
+                            .skip(1)
+                            .next()
+                            .to_owned()
+                            .unwrap()
+                            .to_owned();
+                        if path == "/" {
+                            _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                        } else {
+                            _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").expect("200 \n");
+                        }
+                    }
+                    Ok(_) => {}
+                    Err(e) => println!("Read error: {}", e),
+                };
             }
             Err(e) => {
                 println!("error: {}", e);
