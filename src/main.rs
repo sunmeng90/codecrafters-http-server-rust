b0e14f7f91e02fb579c90fc654bba33f8af0d9bb
Meng <sunmeng90@126.com>
2024-07-06 17:42:59 +0800

    2. Extract URL path: refactor to remove nested match

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{io::{Read, Write}, net::TcpListener};
-
-fn main() {
+use std::net::TcpStream;
+fn main() -> Result<(), std::io::Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -9,33 +9,37 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                let mut buf = [0; 1024];
-                match _stream.read(&mut buf) {
-                    Ok(bytes_read) if bytes_read > 0 => {
-                        let path = String::from_utf8_lossy(&buf[..bytes_read])
-                            .lines()
-                            .next()
-                            .expect("should have first line in request")
-                            .splitn(3, " ")
-                            .skip(1)
-                            .next()
-                            .to_owned()
-                            .unwrap()
-                            .to_owned();
-                        if path == "/" {
-                            _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
-                        } else {
-                            _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").expect("200 \n");
-                        }
-                    }
-                    Ok(_) => {}
-                    Err(e) => println!("Read error: {}", e),
-                };
-            }
+            Ok(_stream) => handle_client(_stream)?,
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let mut buf = [0; 1024];
+    let bytes_read = stream.read(&mut buf)?;
+    if bytes_read == 0 {
+        return Ok(());
+    }
+
+    let path = String::from_utf8_lossy(&buf[..bytes_read])
+        .lines()
+        .next()
+        .expect("should have first line in request")
+        .splitn(3, " ")
+        .skip(1)
+        .next()
+        .to_owned()
+        .unwrap()
+        .to_owned();
+    if path == "/" {
+        stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+    } else {
+        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").expect("200 \n");
+    }
+    Ok(())
 }
